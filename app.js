7e4a139a9c15c7b778740325b8eec6b85df5f9e9
Diana Laura Gonzalez Escamilla <dianathcb@gmail.com>
2025-11-24 23:55:32 -0800

    Completed

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-$(function() {
-  // YOUR CODE HERE
+// 1
+$("#toggleButton").on("click", function() {
+  $(this).next("p").slideToggle()
+})
+// 2
+$("#colorButton").on("click", function() {
+  $(this).next("div").css({backgroundColor: "yellow"})
+})
+// 3
+$("#addClassButton").on("click", function() {
+$(this).next().next("div").addClass("new-class")
+})
+
+$("#removeClassButton").on("click", function() {
+$(this).next("div").removeClass("new-class")
+})
+
+// 4
+$("#fadeInButton").on("click", function() {
+$(this).next().next('div').fadeToggle()
+})
+
+$("#fadeOutButton").on("click", function() {
+$(this).next('div').fadeToggle()
+})
+
+// 5
+$("#slideUpButton").on("click", function() {
+$("#slideDiv").slideUp()
+})
+
+$("#slideDownButton").on("click", function() {
+$(this).next('div').slideDown()
 })
